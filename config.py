ff0d67baf4995b2672bdf682082b38f3d73c9815
zhaoweijie327 <396793472@qq.com>
2020-11-30 09:32:46 +0800

    2020.11.30_获取路径/日志

---
+++ config.py
@@ -0,0 +1,29 @@
+'''
+路径获取
+日志编写
+'''
+import os
+import logging.handlers
+
+# 获取路径
+import time
+
+BAS_URL = os.path.abspath(os.path.dirname(__file__))
+
+# 日志
+def log():
+    # 创建日志器对象
+    rizhi = logging.getLogger()
+    rizhi.setLevel(level=logging.INFO)
+    # 创建处理器
+    lht = logging.handlers.TimedRotatingFileHandler(BAS_URL + '/log/log{}.log'.format(int(time.time() * 1000)),when='M',interval=1,backupCount=2)
+    ls = logging.StreamHandler()
+    # 创建格式化器对象
+    fmt_name = '%(asctime)s %(levelname)s [%(name)s] ' \
+               '[%(filename)s(%(funcName)s:%(lineno)d)] - %(message)s'
+    formatter = logging.Formatter(fmt=fmt_name)
+    # 给处理器设置格式化器
+    lht.setFormatter(formatter)
+    # 给日志器添加处理器
+    rizhi.addHandler(lht)
+    rizhi.addHandler(ls)
